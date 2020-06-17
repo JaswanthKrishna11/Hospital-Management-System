55ac9b27fe0728c30b6a507d268702d61568c658
Sri Raja Vignesh <62837508+sriraja2001@users.noreply.github.com>
2020-06-17 22:14:25 +0530

    Changes final

--- bank_management.c
+++ bank_management.c
@@ -203,7 +203,14 @@ void Cash_withdrawl()
    printf("\nEnter money you want to withdraw from account ");
    scanf("%f",&withdraw_money);
 
-   
+   while (acc_no=account[acc_no-1].acc_number)
+   {
+         account[acc_no-1].available_balance=
+         account[acc_no-1].available_balance-withdraw_money;
+         printf("\nThe New balance for account %d is %f \n",
+         acc_no, account[acc_no-1].available_balance);
+         break;
+   }acc_no++;
 }
 
 
